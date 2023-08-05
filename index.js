55d1901f2a7c76d808a073490d33c2406cf9ad8e
Christopher Vu <christophervu4@gmail.com>
2023-08-04 19:20:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -109,8 +109,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
         buttons.forEach(function(button) {
             if (isCoarsePointer || isNoPointer) {
-                button.style.textShadow = "0px 0px 0px rgba(0, 0, 0, 0)";
-                button.style.boxShadow = 'rgba(0, 0, 0, 0) 0px 5px 10px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px inset';
+                button.style.textShadow = "1px 1px 2px black";
+                button.style.boxShadow = "rgba(0, 0, 0, 0.4) 0px 5px 10px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -12px 0px inset";
             }
             button.style.color = 'white';
             const buttonSpan = button.querySelector('span')
@@ -143,13 +143,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (isNoPointer || isCoarsePointer) {
                 buttons.forEach(function(button) {
-                    button.style.textShadow = "1px 1px 2px black";
-                    button.style.boxShadow = 'rgba(0, 0, 0, 0.4) 0px 5px 10px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -12px 0px inset';
+                    button.style.textShadow = "0px 0px 0px black";
+                    button.style.boxShadow = 'rgba(0, 0, 0, 0) 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px inset';
                     const buttonSpan = button.querySelector('span')
                     button.style.color = 'rgba(0, 0, 0, 0)';
                     button.style.backgroundColor = 'rgba(0, 0, 0, 0)';
                     buttonSpan.style.backgroundColor = 'rgba(255, 255, 255, 0)';
-                    buttonSpan.style.background = 'rgba(255, 255, 255, 0)';
                     buttonSpan.style.border = '4px solid rgba(0, 0, 0, 0)';
                 });
             } else {
